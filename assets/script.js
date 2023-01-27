31e60360be422a98b4408261a470dbec4d1c09f1
RyzaT <ryantaylor50@hotmail.com>
2023-01-27 12:09:32 +0000

    copied criteria and added api key

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+// OpenWeather API,
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=e58a91b5528ac74b5640933904b043fa";
+
+
+
+// When a user views the current weather conditions for that city they are presented with:
+
+// The city name
+
+// The date
+
+// An icon representation of weather conditions
+
+// The temperature
+
+// The humidity
+
+// The wind speed
+
+// When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+
+// The date
+
+// An icon representation of weather conditions
+
+// The temperature
+
+// The humidity
+
+// When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
